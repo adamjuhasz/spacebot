26d6eca08ec435a36db0a42338416b610a989d0b
bot-maker <none>
2017-04-11 21:00:04 +0000

    Saved with alana.cloud

--- scripts/greeter.js
+++ scripts/greeter.js
@@ -1,5 +1,6 @@
 // Created Tue Apr 11 2017
 addGreeting(function(user, response){
+    console.log('greeting run');
     response
         .sendText('Welcome to spacebot')
         .sendText('I love space!');
