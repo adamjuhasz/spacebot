03153fa91aae6c1ea9db535f31c1ab3cba8a0ec5
bot-maker <none>
2017-04-11 21:52:57 +0000

    Saved with alana.cloud

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,12 @@
 // Created Tue Apr 11 2017
 newScript('profile')
-    .dialog((session, response) => {
+    .dialog('nickname', (session, response) => {
         response.sendText('What should I call you?');
     })
+    .expect
+        .text((session, response) => {
+            session.user.state.name = session.message.text;
+        })
+        .catch((session, response) => {
+            response.goto('nickname');
+        })
