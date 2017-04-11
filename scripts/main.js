f1658914ef6e04372b356ca49b0ce621930e0b97
bot-maker <none>
2017-04-11 21:06:21 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,12 @@
 // getScript
 // addGreeting
 // request
+
+newScript()
+    .dialog(function(session, response) {
+        response
+            .createButtons()
+            .text('Pick an option')
+            .button('postback', 'Photo of the day', 'POTD')
+            .send();
+    })
