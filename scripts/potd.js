a1bd59f935fa149e437ecdb1eea74f738e011a54
bot-maker <none>
2017-04-12 15:31:02 +0000

    Saved with alana.cloud

--- scripts/potd.js
+++ scripts/potd.js
@@ -1,2 +1,15 @@
 // Created Tue Apr 11 2017
 newScript('POTD')
+    .dialog((session, response) => {
+        return request({
+            uri: 'https://api.nasa.gov/planetary/apod',
+            method: 'GET',
+            qs: {
+                api_key: 'DEMO_KEY'
+            },
+            json: true,
+        })
+        .then(apod => {
+            response.sendImage(apod.url);
+        })
+    })
