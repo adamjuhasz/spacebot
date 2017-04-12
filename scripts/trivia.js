7f92f66ebd9114ba805f3e46774744c65d7d716c
bot-maker <none>
2017-04-12 15:43:31 +0000

    Saved with alana.cloud

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,1 +1,20 @@
 // Created Wed Apr 12 2017
+
+[{
+    question: 'What type of galaxy is the most common in the universe?',
+    correct: 'Elliptical',
+    wrong: ['Spiral','Irregular']
+},
+{
+    question: 'What is the coldest place in the universe?',
+    correct: 'Boomerang Nebula',
+    wrong: ['Center of the Milky Way','Moon\'s surface', 'Pluto\'s surface'],    
+},
+{
+    question: 'How old is the universe in light years?',
+    correct: '13.8',
+    wrong: ['1.4', '60', '76'],    
+}]
+
+newScript('trivia')
+    .
