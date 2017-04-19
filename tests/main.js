7d9157d1486ad41ac4fdefd11dfc7c642c1b2e00
bot-maker <none>
2017-04-19 20:31:08 +0000

    Saved with alana.cloud

--- tests/main.js
+++ tests/main.js
@@ -17,3 +17,20 @@ test('profile', function(){
         ])
         .run();
 });
+
+function doGreeting(test) {
+    test.expectText('Welcome to spacebot')
+        .expectText('I love space!')
+        .expectText('What should I call you?')
+        .sendText('bot tester')
+        .expectButtons('Hello bot tester, what do you want to do?', [
+            { type: 'postback', text: 'Photo of the day', payload: 'POTD' },
+            { type: 'postback', text: 'Space trivia', payload: 'TRIVIA' },
+        ]);
+}
+
+test('auto-do', function(){
+    const test = newTest()
+    doGreeting(test);
+    return rest.run();
+});
