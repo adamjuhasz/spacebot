5d07fc5620ff97e024554853b991e949a702c2fa
bot-maker <none>
2017-04-19 21:48:57 +0000

    Saved with alana.cloud

--- tests/main.js
+++ tests/main.js
@@ -48,6 +48,6 @@ test('potd mock', function(){
     doGreeting(test);
     test.sendButtonClick('POTD')
         .expectImage('https://image.jpg')
-    return test.run();
+    return rest.run();
 });
 
